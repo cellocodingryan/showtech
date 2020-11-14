05dae7f1d5735c041d7fbb29763ae96941524651
cellocodingryan <cellocodingryan@gmail.com>
2020-11-14 17:25:59 -0500

    reverted run .sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-#docker pull mongo
-#docker pull node
-#docker network create showtech-network
-#docker run -d --network showtech-network -p 27017:27017 --rm --name database mongo
+docker pull mongo
+docker pull node
+docker network create showtech-network
+docker run -d --network showtech-network -p 27017:27017 --rm --name database mongo
 docker build . -t showtech_web
-sudo docker stop showtechs
-sudo docker run -it  --network showtech-network --name showtechs -p 5000:5000 --rm showtech_web
+docker stop showtechs
+docker run -it  --network showtech-network --name showtechs -p 5000:5000 --rm showtech_web
